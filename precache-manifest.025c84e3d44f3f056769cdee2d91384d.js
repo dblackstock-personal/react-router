465f41920c0c00d576260f26fa599c7b446195e3
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-26 17:21:07 +0000

    Updates

---
+++ precache-manifest.025c84e3d44f3f056769cdee2d91384d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df225312ffb5ec994850733245bc33dc",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "c4febe306922f2f2ac84",
+    "url": "/react-router/static/css/main.ad7b03fe.chunk.css"
+  },
+  {
+    "revision": "183c32893defda304f8d",
+    "url": "/react-router/static/js/2.f336a1de.chunk.js"
+  },
+  {
+    "revision": "c4febe306922f2f2ac84",
+    "url": "/react-router/static/js/main.034f0b85.chunk.js"
+  },
+  {
+    "revision": "513236836e2a7859257f",
+    "url": "/react-router/static/js/runtime-main.43025125.js"
+  }
+]);
